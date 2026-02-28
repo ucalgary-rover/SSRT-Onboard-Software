dfaa99de537aae0fade20f0c92f3afb605f0edda
Hasan Salhi <hasan.salhi2006@gmail.com>
2026-02-28 11:07:50 -0700

    h

---
+++ MQTTServer/src/sensors/imu.cpp
@@ -0,0 +1,39 @@
+#include "sensors/imu.hpp"
+
+#include <filesystem>  // needed for runtime_error
+
+IMUSensor::IMUSensor(const std::string& topic, std::chrono::milliseconds update_interval)
+    : SensorBase(topic), m_update_interval(update_interval) {}
+
+float IMUDimensions[3]; // Pitch, Roll, Yaw
+
+float IMUSensor::generate_data() {
+    return (std::rand() % 721) - 360.0f;  // generate a random float between -360 and 360
+}
+
+float IMUSensor::read_data() {
+    throw std::runtime_error(
+        "This is a sample class with no real sensor attached. There is no data to be read.");
+
+    return 0;
+}
+
+void IMUSensor::sensor_loop() {
+    while (m_running) {
+        // get data
+        for (int i = 0; i < 3; ++i) {
+            IMUDimensions[i] = generate_data();
+        }
+
+        // publish data
+        if (m_callback) {
+            std::string payload = "Pitch: " + std::to_string(IMUDimensions[0]) +
+                                  ", Roll: " + std::to_string(IMUDimensions[1]) +
+                                  ", Yaw: " + std::to_string(IMUDimensions[2]);
+            m_callback(m_topic, payload);
+        }
+
+        // wait for more data
+        std::this_thread::sleep_for(m_update_interval);
+    }
+}
