5c300fef1c7a17d372a8af74f7030916999dfe45
Asim424 <asimhamdan2005@gmail.com>
2026-03-07 11:54:50 -0700

    getting the IMUDataParse on the git

--- MQTTServer/include/sensors/imu.hpp
+++ MQTTServer/include/sensors/imu.hpp
@@ -7,16 +7,17 @@ class IMUSensor : public SensorBase {
 private:
     std::chrono::milliseconds m_update_interval;
     float m_imu_rpy[3]; // Roll, Pitch, Yaw
+    float* m_rpy_out;
 
 protected:
     void sensor_loop() override;
 
 public:
     explicit IMUSensor(const std::string& topic, std::chrono::milliseconds update_interval =
-                                                        std::chrono::milliseconds(500));
+                                                        std::chrono::milliseconds(500), float* m_rpy_out);
 
     // single read/generate methods that can be called individually as needed
-    float read_data();
+    void read_data();
     float generate_data();
 };
 
--- MQTTServer/src/sensors/imu.cpp
+++ MQTTServer/src/sensors/imu.cpp
@@ -2,18 +2,16 @@
 
 #include <filesystem>  // needed for runtime_error
 
-IMUSensor::IMUSensor(const std::string& topic, std::chrono::milliseconds update_interval)
-    : SensorBase(topic), m_update_interval(update_interval) {}
+IMUSensor::IMUSensor(const std::string& topic, std::chrono::milliseconds update_interval, float* m_rpy_out)
+    : SensorBase(topic), m_update_interval(update_interval), m_rpy_out(m_rpy_out) {}
 
 float IMUSensor::generate_data() {
     return (std::rand() % 721) - 360.0f;  // generate a random float between -360 and 360
 }
 
-float IMUSensor::read_data() {
+void IMUSensor::read_data() {
     throw std::runtime_error(
         "This is a sample class with no real sensor attached. There is no data to be read.");
-
-    return 0;
 }
 
 void IMUSensor::sensor_loop() {
@@ -25,8 +23,8 @@ void IMUSensor::sensor_loop() {
 
         // publish data
         if (m_callback) {
-            std::string payload = "Pitch: " + std::to_string(m_imu_rpy[0]) +
-                                  ", Roll: " + std::to_string(m_imu_rpy[1]) +
+            std::string payload = "Roll: " + std::to_string(m_imu_rpy[0]) +
+                                  ", Pitch: " + std::to_string(m_imu_rpy[1]) +
                                   ", Yaw: " + std::to_string(m_imu_rpy[2]);
             m_callback(m_topic, payload);
         }
